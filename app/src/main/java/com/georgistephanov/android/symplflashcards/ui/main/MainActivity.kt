d03dfb9cd8c7a3a3ad34a76c23c7678e2ed8953d
Georgi Stefanov <georgistephanov@gmail.com>
2018-03-17 15:38:56 +0000

    Created the initial adapter which is going to display the stacks on the main activity

--- app/src/main/java/com/georgistephanov/android/symplflashcards/ui/main/MainActivity.kt
+++ app/src/main/java/com/georgistephanov/android/symplflashcards/ui/main/MainActivity.kt
@@ -1,15 +1,17 @@
-package com.georgistephanov.android.symplflashcards
+package com.georgistephanov.android.symplflashcards.ui.main
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ListView
+import com.georgistephanov.android.symplflashcards.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -18,9 +20,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val mainList = mutableListOf<MainListRow>()
+        mainList.add(0, MainListRow("Uni", 6))
+        mainList.add(1, MainListRow("Didi", 18))
+        mainList.add(2, MainListRow("Didi", 1))
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            find<ListView>(R.id.main_list).adapter = MainListAdapter(this, mainList)
         }
 
         val toggle = ActionBarDrawerToggle(
@@ -40,15 +46,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
             R.id.action_settings -> return true
             else -> return super.onOptionsItemSelected(item)
